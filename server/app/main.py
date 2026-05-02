d149324acaa49771761281ce98f2b2ad2fa47afb
bloody-k <olegpetlya@mail.ru>
2026-05-02 17:48:45 +0300

    Добавил домен фронтенда в CORS

--- server/app/main.py
+++ server/app/main.py
@@ -19,7 +19,10 @@ app = FastAPI(title="Art Platform", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://art-platform-1.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
