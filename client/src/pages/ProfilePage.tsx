164f3e5c57688820f4ec2ef7e3a85a393f3a3d8e
bloody-k <olegpetlya@mail.ru>
2026-05-13 03:47:57 +0300

    кнопка добавить изображение  перемещена в профиль

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -182,6 +182,9 @@ export default function ProfilePage() {
       </div>
 
       <h2 className="mt-5">Мои работы</h2>
+      <div className="text-end mb-3">
+        <Link to="/upload" className="btn btn-outline-light btn-sm">+ Добавить работу</Link>
+      </div>
       {artworks.map((a) => (
         <div key={a.id} className="mb-4 border rounded">
           <div className="d-flex align-items-center p-2">
