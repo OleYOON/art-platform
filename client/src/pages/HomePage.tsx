8f7da5b7a66f22249eb21fc9972acc76245d4b12
bloody-k <olegpetlya@mail.ru>
2026-05-13 03:56:02 +0300

    кнопкf поиска перемещена

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -71,7 +71,7 @@ export default function HomePage() {
       <div className="d-flex justify-content-between align-items-center py-3 border-bottom mb-0 sticky-top">
         <h4 className="m-0">paws</h4>
         <div className="d-flex align-items-center">
-          <div className="input-group input-group-sm me-2" style={{ maxWidth: 150 }}>
+          <div className="input-group input-group-sm flex-grow-1 mx-auto" style={{ maxWidth: 300 }}>
             <input type="text" className="form-control" placeholder="Поиск..." value={search} onChange={e => setSearch(e.target.value)} />
             {search && <button className="btn btn-outline-secondary" onClick={() => setSearch("")}>✕</button>}
           </div>
